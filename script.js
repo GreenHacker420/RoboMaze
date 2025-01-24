fe388cb61abe5e3de4d128489c8d4b4d0917bd83
Harsh Hirawat <106510913+GreenHacker420@users.noreply.github.com>
2025-01-24 13:26:41 +0530

    submission close

--- script.js
+++ script.js
@@ -18,3 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Submission is closed.');
+});
